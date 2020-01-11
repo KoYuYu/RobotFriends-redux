a7b96064b2df876d9f6a9f09230d33e60bb4144c
YuYu <jasonko12033@gmail.com>
2020-01-11 13:51:19 -0500

    Updates

---
+++ precache-manifest.fd0467c516a0b09935780db7ec07c6ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cd4322f89459c9f35ee36ced0c45d4",
+    "url": "/RobotFriends-redux/index.html"
+  },
+  {
+    "revision": "ccec3c18278b320eeb5e",
+    "url": "/RobotFriends-redux/static/css/2.1e00258c.chunk.css"
+  },
+  {
+    "revision": "f7e1980ef1eb2398971f",
+    "url": "/RobotFriends-redux/static/css/main.308924a7.chunk.css"
+  },
+  {
+    "revision": "ccec3c18278b320eeb5e",
+    "url": "/RobotFriends-redux/static/js/2.85dbaee8.chunk.js"
+  },
+  {
+    "revision": "0da7a4f22d3c4a94734c6af8fa17fc54",
+    "url": "/RobotFriends-redux/static/js/2.85dbaee8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7e1980ef1eb2398971f",
+    "url": "/RobotFriends-redux/static/js/main.a28bc49b.chunk.js"
+  },
+  {
+    "revision": "a6b4ee6d5f70fb8d9c6f",
+    "url": "/RobotFriends-redux/static/js/runtime-main.2993e4b8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
